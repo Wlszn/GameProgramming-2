4ceb55b183711e69b10458ce391fa3ae543c6847
Will <6236388@edu.vaniercollege.qc.ca>
2026-09-17 09:04:23 -0400

    Finished task 4 of the lab

--- lab04/Assets/Script/GunComponent.cs
+++ lab04/Assets/Script/GunComponent.cs
@@ -9,5 +9,28 @@ public class GunComponent : MonoBehaviour
     private float chargeTime = 0.0f;
     private bool isCharging = false;
 
-   
+    void Update()
+    {
+
+        if (Input.GetButtonUp("Fire1"))
+        {
+            ShootBullet();
+        }
+
+        while (Input.GetButtonDown("Fire1"))
+        {
+            ShootBullet();
+        }
+
+    }
+
+    void ShootBullet()
+    {
+        GameObject bullet = Instantiate(bulletPrefab, bulletSpawnPoint.position, bulletSpawnPoint.rotation);
+        Rigidbody rb = bullet.GetComponent<Rigidbody>();
+
+        float bulletImpulse = bulletMaxImpulse * (chargeTime / maxChargeTime);
+
+        rb.AddForce(bulletSpawnPoint.forward * bulletImpulse, ForceMode.Impulse);
+    }
 }
